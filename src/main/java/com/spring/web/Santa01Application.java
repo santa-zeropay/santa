4ab259e6f528d33593312ba5a8b36a06f77c0825
Subin Lee <59813122+binibinii@users.noreply.github.com>
2021-06-26 14:50:43 +0900

    mysql 및 jsp  설정

---
+++ src/main/java/com/spring/web/Santa01Application.java
@@ -0,0 +1,13 @@
+package com.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santa01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/web/controller/testController.java
@@ -0,0 +1,13 @@
+package com.spring.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+
+    @RequestMapping("/hello")
+    public void main() {
+       
+    }
+}
